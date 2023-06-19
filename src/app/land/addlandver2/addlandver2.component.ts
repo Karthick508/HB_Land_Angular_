42ac322cbf547ca85589c76de2bf736c6a7bd70b
bocxy <sapna@bocxy.com>
2023-06-19 20:27:05 +0530

    first commit

---
+++ src/app/land/addlandver2/addlandver2.component.ts
@@ -0,0 +1,515 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
+
+
+@Component({
+  selector: 'app-addlandver2',
+  templateUrl: './addlandver2.component.html',
+  styleUrls: ['./addlandver2.component.css']
+})
+export class Addlandver2Component {
+
+
+  isPanel1Expanded = false;
+  isPanel2Expanded = false;
+  isPanel3Expanded = false;
+  isPanel4Expanded = false;
+  LPSFormGroup: FormGroup;
+  expansionPanelsArray: FormArray;
+
+  @ViewChild('stepper') stepper: MatStepper;
+
+  index = 0;
+
+  togglePanel1() {
+    this.isPanel1Expanded = !this.isPanel1Expanded;
+  }
+
+  togglePanel2() {
+    this.isPanel2Expanded = !this.isPanel2Expanded;
+  }
+  togglePanel3() {
+    this.isPanel2Expanded = !this.isPanel2Expanded;
+  }
+   togglePanel4() {
+    this.isPanel2Expanded = !this.isPanel2Expanded;
+  }
+
+  personalInfoFormGroup!: FormGroup;
+
+  fourOneFormGroup: FormGroup;
+  expansionPanelsArray4: FormArray;
+
+  sixDDFormGroup:FormGroup;
+  expansionPanelsSixDD:FormArray;
+
+  awardInfoFormGroup!: FormGroup;
+  expansionPanelsAward : FormArray
+
+  constructor(private builder: FormBuilder, private formBuilder: FormBuilder) { }
+  isLinear = true;
+
+  ngOnInit(): void {
+
+
+
+
+    this.personalInfoFormGroup = this.formBuilder.group({
+      v_name_of_circle: ['', Validators.required],
+      v_name_of_scheme: ['', Validators.required],
+      v_name_of_division: ['', Validators.required],
+      v_name_of_geo_tagging: ['', Validators.required],
+      v_name_of_district: ['', Validators.required],
+
+    });
+
+
+
+  // LPS FORM
+    this.LPSFormGroup = this.formBuilder.group({
+      expansionPanels: this.formBuilder.array([])
+    });
+
+    this.expansionPanelsArray = this.LPSFormGroup.get('expansionPanels') as FormArray;
+    this.addExpansionPanel();
+
+// 4one
+    this.fourOneFormGroup = this.formBuilder.group({
+      expansionPanels4: this.formBuilder.array([])
+    });
+
+    this.expansionPanelsArray4 = this.fourOneFormGroup.get('expansionPanels4') as FormArray;
+    this.addExpansionPanel4One();
+
+    // 6DD
+    this.sixDDFormGroup = this.formBuilder.group({
+      expansionPanelsSix: this.formBuilder.array([])
+    });
+
+    this.expansionPanelsSixDD = this.sixDDFormGroup.get('expansionPanelsSix') as FormArray;
+    this.addExpansionPanelSixDD();
+
+   // Award
+    this.awardInfoFormGroup = this.formBuilder.group({
+      awardsDet: this.formBuilder.array([]),
+    });
+
+    // this.expansionPanelsAward = this.awardInfoFormGroup.get('expansionPanelsAward') as FormArray;
+    // this.addExpansionPanelAward();
+
+
+  }
+
+
+
+  addExpansionPanel4One() {
+    const expansionPanel4One = this.formBuilder.group({
+      file: null,
+      file2: null,
+      v_gazette_ref_no: [''],
+      v_4_one_go_ref_no: [''],
+      d_date_of_4_one_go: [''],
+      d_date_of_gazette_notification: [''],
+      d_date_of_locality	: [''],
+      v_ref_no: [''],
+      v_total_extent: [''],
+      repeatedFields: this.formBuilder.array([]),
+      ownerFields : this.formBuilder.array([])
+
+    });
+
+    this.expansionPanelsArray4.push(expansionPanel4One);
+  }
+
+  addExpansionPanelSixDD() {
+    const expansionPanelSix = this.formBuilder.group({
+      file: null,
+      file2: null,
+      v_gazette_ref_no: [''],
+      v_6dd_go_ref_no: [''],
+      d_date_of_6dd_go: [''],
+      d_date_of_gazette_notification: [''],
+      d_date_of_locality: [''],
+      v_ref_no: [''],
+      v_total_extent: [''],
+      repeatedFields: this.formBuilder.array([]),
+      sixddFields: this.formBuilder.array([])
+    });
+    this.expansionPanelsSixDD.push(expansionPanelSix);
+  }
+
+  // addExpansionPanelAward() {
+  //   const expansionPanelAward = this.formBuilder.group({
+
+  //   });
+  //   this.expansionPanelsAward.push(expansionPanelAward);
+  // }
+
+
+
+  removeLastRepeatedField(type) {
+    if (this.expansionPanelsArray.length > 0 && type === 'lps') {
+      this.expansionPanelsArray.removeAt(this.expansionPanelsArray.length - 1);
+    }  else if (this.expansionPanelsArray4.length > 0 && type === '4(1)') {
+      this.expansionPanelsArray4.removeAt(this.expansionPanelsArray4.length - 1);
+    }else if(this.expansionPanelsSixDD.length > 0 && type ==='6DD'){
+      this.expansionPanelsSixDD.removeAt(this.expansionPanelsSixDD.length - 1);
+    }
+  }
+
+  addExpansionPanel() {
+    const expansionPanel = this.formBuilder.group({
+      file: null,
+      v_ref_no: [''],
+      v_total_extent: [''],
+      repeatedFields: this.formBuilder.array([]),
+      villageFields: this.formBuilder.array([])
+
+    });
+    this.expansionPanelsArray.push(expansionPanel);
+  }
+
+  onFileChange(event: any, panelIndex: number, type) {
+    const file = event.target.files && event.target.files.length > 0 ? event.target.files[0] : null;
+    if (type === 'lps') {
+      const panelFormGroup = this.expansionPanelsArray.at(panelIndex) as FormGroup;
+      if (file) {
+        const prefix = type; // Specify your desired prefix here
+        const suffix = panelIndex
+        const fileNameWithPrefix = prefix + '_' + file.name + '_' + suffix;
+        const prefixedFile = new File([file], fileNameWithPrefix, { type: file.type });
+        panelFormGroup.get('file').setValue(prefixedFile);
+      } else {
+        panelFormGroup.get('file').setValue(null);
+      }
+    } else if(type=== '4(1)'){
+      const panelFormGroup = this.expansionPanelsArray4.at(panelIndex) as FormGroup;
+      if (file) {
+        const prefix = type; // Specify your desired prefix here
+        const suffix = panelIndex
+        const fileNameWithPrefix = prefix + '_' + file.name + '_' + suffix;
+        const prefixedFile = new File([file], fileNameWithPrefix, { type: file.type });
+        panelFormGroup.get('file').setValue(prefixedFile);
+      } else {
+        panelFormGroup.get('file').setValue(null);
+      }
+    }else if(type ==='6DD'){
+      const panelFormGroup = this.expansionPanelsSixDD.at(panelIndex) as FormGroup;
+      if (file) {
+        const prefix = type; // Specify your desired prefix here
+        const suffix = panelIndex
+        const fileNameWithPrefix = prefix + '_' + file.name + '_' + suffix;
+        const prefixedFile = new File([file], fileNameWithPrefix, { type: file.type });
+        panelFormGroup.get('file').setValue(prefixedFile);
+    }else {
+      panelFormGroup.get('file').setValue(null);
+    }
+  }
+
+  }
+
+  submit() {
+    console.log(this.personalInfoFormGroup.value);
+    console.log(this.LPSFormGroup.value)
+
+    console.log(this.fourOneFormGroup.value)
+     console.log(this.sixDDFormGroup.value)
+  }
+  removeExpansionPanel(index: number, type) {
+    if (type === 'lps') {
+      this.expansionPanelsArray.removeAt(index);
+    } else if(type === '4(1)'){
+      this.expansionPanelsArray4.removeAt(index)
+    }else if(type === '6DD'){
+      this.expansionPanelsSixDD.removeAt(index)
+    }
+  }
+
+  addRepeatedField(expansionPanelIndex: number, type) {
+    let expansionPanel;
+    let repeatedFieldsArray;
+    if (type === 'lps') {
+      expansionPanel = this.expansionPanelsArray.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    } else if(type === '4(1)'){
+      expansionPanel = this.expansionPanelsArray4.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    }else if(type === '6DD'){
+      expansionPanel = this.expansionPanelsSixDD.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    }
+    const repeatedField = this.formBuilder.group({
+      field1: [''],
+      field2: ['']
+    });
+
+    repeatedFieldsArray.push(repeatedField);
+  }
+
+  addVillageLps(expansionPanelIndex: number) {
+    let expansionPanel;
+    let villageFieldsArray;
+     {
+      expansionPanel = this.expansionPanelsArray.at(expansionPanelIndex) as FormGroup;
+      villageFieldsArray = expansionPanel.get('villageFields') as FormArray;
+    }
+    const villageField = this.formBuilder.group({
+      v_name_of_village: [''],
+      v_survey_no: [''],
+      v_extent: ['']
+    });
+
+    villageFieldsArray.push(villageField);
+  }
+
+  addOwner4one(expansionPanelIndex: number) {
+    const expansionPanel = this.expansionPanelsArray4.at(expansionPanelIndex) as FormGroup;
+    const ownerFieldsArray = expansionPanel.get('ownerFields') as FormArray;
+    const ownerField = this.formBuilder.group({
+      v_survey_no: [''],
+      v_extent: [''],
+      v_name_of_owner: [''],
+      v_east: [''],
+      v_west: [''],
+      v_north: [''],
+      v_south: [''],
+    });
+
+    ownerFieldsArray.push(ownerField);
+  }
+
+
+
+  addsixdd(expansionPanelIndex: number) {
+    const expansionPanel = this.expansionPanelsSixDD.at(expansionPanelIndex) as FormGroup;
+    const sixddFieldsArray = expansionPanel.get('sixddFields') as FormArray;
+
+    const sixddField = this.formBuilder.group({
+      v_survey_no: [''],
+      v_extent: ['']
+    });
+
+    sixddFieldsArray.push(sixddField);
+  }
+
+  removeRepeatedField(expansionPanelIndex: number, repeatedFieldIndex: number, type) {
+    let expansionPanel;
+    let repeatedFieldsArray;
+    if (type === 'lps') {
+      expansionPanel = this.expansionPanelsArray.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    } else if (type === '4(1)') {
+      expansionPanel = this.expansionPanelsArray4.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    }else if (type === '6DD') {
+      expansionPanel = this.expansionPanelsSixDD.at(expansionPanelIndex) as FormGroup;
+      repeatedFieldsArray = expansionPanel.get('repeatedFields') as FormArray;
+    }
+
+    repeatedFieldsArray.removeAt(repeatedFieldIndex);
+  }
+
+  removeVillageLps(expansionPanelIndex: number, villageFieldIndex: number) {
+    let expansionPanel;
+    let villageFieldsArray;
+     {
+      expansionPanel = this.expansionPanelsArray.at(expansionPanelIndex) as FormGroup;
+      villageFieldsArray = expansionPanel.get('villageFields') as FormArray;
+    }
+
+    villageFieldsArray.removeAt(villageFieldIndex);
+  }
+
+  removeOwner4one(expansionPanelIndex: number, ownerFieldIndex: number) {
+
+    const  expansionPanel = this.expansionPanelsArray4.at(expansionPanelIndex) as FormGroup;
+    const  ownerFieldsArray = expansionPanel.get('ownerFields') as FormArray;
+
+
+    ownerFieldsArray.removeAt(ownerFieldIndex);
+  }
+
+  removesixdd(expansionPanelIndex: number, sixddFieldIndex: number) {
+    const expansionPanel = this.expansionPanelsSixDD.at(expansionPanelIndex) as FormGroup;
+    const sixddFieldsArray = expansionPanel.get('sixddFields') as FormArray;
+
+    sixddFieldsArray.removeAt(sixddFieldIndex);
+  }
+
+
+
+
+
+
+  panelOpenState = false;   // mat expansion panel in Lps
+
+  // Lps input field - start
+  lpsFields: any[] = [];
+
+  addLpsField() {
+    const newLpsField = 'New Lps Field';
+    this.lpsFields.push(newLpsField);
+  }
+
+  removeLpsField(index: number) {
+    this.lpsFields.splice(index, 1);
+  }
+  // Lps input field - end
+
+  // Lps form addition - start
+  lpsForms: any[] = [];
+
+  addLpsForm() {
+    const newLpsIndex = this.lpsForms.length + 2;
+    const newLpsForm = { index: newLpsIndex, value: '' };
+    this.lpsForms.push(newLpsForm);
+  }
+
+  lpsFields2: any[] = [];
+
+  addLpsField2() {
+    const newLpsField2 = 'New Lps Field2';
+    this.lpsFields2.push(newLpsField2);
+  }
+
+  removeLpsField2(index: number) {
+    this.lpsFields2.splice(index, 1);
+  }
+
+  adds: any[] = [];
+
+  addsurvey() {
+    const addsurvey = 'Add Survey Field';
+    this.adds.push(addsurvey);
+  }
+
+  removeSurvey(index: number) {
+    this.adds.splice(index, 1);
+  }
+
+  adds2: any[] = [];
+
+  addsurvey2() {
+    const addsurvey2 = 'Add Survey Field2';
+    this.adds2.push(addsurvey2);
+  }
+
+  removeSurvey2(index: number) {
+    this.adds2.splice(index, 1);
+  }
+
+
+
+
+  // Lps form addition - end
+
+
+
+  // Four input field - start
+  fourFields: any[] = [];
+
+  addFourField() {
+    const newFourField = 'New Four Field';
+    this.fourFields.push(newFourField);
+  }
+
+  removeFourField(index: number) {
+    this.fourFields.splice(index, 1);
+  }
+  // Four input field - end
+
+  // Four form addition - start
+  fourForms: any[] = [];
+
+  addFourForm() {
+    const newFourIndex = this.fourForms.length + 2;
+    const newFourForm = { index: newFourIndex, value: '' };
+    this.fourForms.push(newFourForm);
+  }
+
+  fourFields2: any[] = [];
+
+  addFourField2() {
+    const newFourField2 = 'New Four Field2';
+    this.fourFields2.push(newFourField2);
+  }
+
+  removeFourField2(index: number) {
+    this.fourFields2.splice(index, 1);
+  }
+  // Four form addition - end
+
+
+  // Dd input field - start
+  ddFields: any[] = [];
+
+  addDdField() {
+    const newDdField = 'New Dd Field';
+    this.ddFields.push(newDdField);
+  }
+
+  removeDdField(index: number) {
+    this.ddFields.splice(index, 1);
+  }
+  // Dd input field - end
+
+  // Dd form addition - start
+  ddForms: any[] = [];
+
+  addDdForm() {
+    const newDdIndex = this.ddForms.length + 2;
+    const newDdForm = { index: newDdIndex, value: '' };
+    this.ddForms.push(newDdForm);
+  }
+
+  ddFields2: any[] = [];
+
+  addDdField2() {
+    const newDdField2 = 'New Dd Field2';
+    this.ddFields2.push(newDdField2);
+  }
+
+  removeDdField2(index: number) {
+    this.ddFields2.splice(index, 1);
+  }
+  // Dd form addition - end
+
+
+  //left over add-start
+  leftForms1: string[] = [];
+  leftForms2: string[] = [];
+  leftForms3: string[] = [];
+
+  addLeftForm1() {
+    const newLeftForm1 = 'New Left Form1';
+    this.leftForms1.push(newLeftForm1);
+  }
+
+  addLeftForm2() {
+    const newLeftForm2 = 'New Left Form2';
+    this.leftForms2.push(newLeftForm2);
+  }
+
+  addLeftForm3() {
+    const newLeftForm3 = 'New Left Form3';
+    this.leftForms3.push(newLeftForm3);
+  }
+  //left over add-end
+
+  //left over remove-start
+  removeLeftForm1(index: number) {
+    this.leftForms1.splice(index, 1);
+  }
+
+  removeLeftForm2(index: number) {
+    this.leftForms2.splice(index, 1);
+  }
+
+  removeLeftForm3(index: number) {
+    this.leftForms3.splice(index, 1);
+  }
+  //left over remove-end
+
+
+}
---
+++ src/app/land/land.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LandComponent } from './land.component';
+import { HomeComponent } from './home/home.component';
+import { ViewlandComponent } from './viewland/viewland.component';
+import { AddlandComponent } from './addland/addland.component';
+import { EditlandComponent } from './editland/editland.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { SharedModule } from '../shared-module/shared.module';
+import { Addlandver2Component } from './addlandver2/addlandver2.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LandComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full',
+
+              },            { path: 'view/:id', component: ViewlandComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'addver2', component: Addlandver2Component },
+        ],
+    },
+];
+@NgModule({
+    declarations: [LandComponent, ViewlandComponent, HomeComponent,    AddlandComponent,
+        EditlandComponent,
+        Addlandver2Component
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule,MatIconModule,MatTableModule,
+        RouterModule.forChild(routes), ReactiveFormsModule, FormsModule,SharedModule
+    ]
+})
+export class LandModule { }
